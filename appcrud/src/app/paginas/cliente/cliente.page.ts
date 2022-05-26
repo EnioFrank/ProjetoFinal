45649da0369843c3fdd57233b4a7941d6f352548
EnioFrank <enio.jiparana@gmail.com>
2022-05-25 20:39:29 -0400

    segundosave

--- appcrud/src/app/paginas/cliente/cliente.page.ts
+++ appcrud/src/app/paginas/cliente/cliente.page.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Cliente } from 'src/app/servicos/cliente.service';
 @Component({
   selector: 'app-cliente',
   templateUrl: './cliente.page.html',
   styleUrls: ['./cliente.page.scss'],
 })
 export class ClientePage implements OnInit {
-
+  clientes: Cliente[];
   constructor() { }
 
   ngOnInit() {
--- appcrud/src/app/servicos/cliente.service.ts
+++ appcrud/src/app/servicos/cliente.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
+export interface Cliente{
+  id: number;
+  nome: string;
+  telefone: string;
+  endereco: string;
+}
 @Injectable({
   providedIn: 'root'
 })
+
 export class ClienteService {
 
-  constructor() { }
+  private url = 'http://localhost/apiAppCrud/apiCliente.php';
+
+  constructor(private http: HttpClient) { }
+
+  getAll(){
+    return this.http.get<[Cliente]>(this.url);
+  }
 }
